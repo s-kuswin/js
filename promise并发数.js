79b0fa4bfb6e356ad46e250dd67d8c48dfe3183f
输入你的用户名 <>
2020-07-28 10:20:32 +0800

    promise控制并发数

---
+++ promise并发数.js
@@ -0,0 +1,29 @@
+class requestList {
+    constructor (limit) {
+        this.limit = limit || 2;
+        this.waitQueue = [];
+        this.currentNum = 0
+
+    }
+    async request(req) {
+        if (!req) throw new Error('req is required.')
+        if (Object.prototype.toString.call(req) !== '[object Function]') throw new Error ('Req must be a function.')
+        if (this.currentNum >= this.limit) {
+            await new Promise ((resolve) => this.waitQueue.push(resolve))
+        }
+        return handleReq(req)
+    }
+    async handleReq(req) {
+        this.currentNum++
+        try{
+            return await req()
+        } catch (err){
+            return Promise.reject(err)
+        }finally {
+            this.currentNum--
+            this.waitQueue[0]()
+            this.waitQueue.shift()
+        }
+
+    }
+}
